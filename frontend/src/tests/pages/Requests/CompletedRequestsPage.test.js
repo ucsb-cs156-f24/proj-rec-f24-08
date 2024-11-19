c9d3aad27f5944b457d61b88dd229654b576bd71
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-19 15:21:02 -0800

    creating fixtures

--- frontend/src/tests/pages/Requests/CompletedRequestsPage.test.js
+++ frontend/src/tests/pages/Requests/CompletedRequestsPage.test.js
@@ -23,21 +23,123 @@ describe("CompletedRequestsPage tests", () => {
   };
 
   const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
 
+  test("renders three recommendations correctly for regular user", async () => {
     setupUserOnly();
+    axiosMock
+      .onGet("/api/restaurants/all")
+      .reply(200, restaurantFixtures.threeRestaurants);
 
-    // act
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
-          <CompletedRequestsPage />
+          <RestaurantIndexPage />
         </MemoryRouter>
       </QueryClientProvider>,
     );
 
-    // assert
-    await screen.findByText("Completed Requests page not yet implemented");
+    await waitFor(() => {
+      expect(
+        screen.getByTestId(`${testId}-cell-row-0-col-id`),
+      ).toHaveTextContent("2");
+    });
+    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
+      "3",
+    );
+    expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent(
+      "4",
+    );
+
+    const createRestaurantButton = screen.queryByText("Create Restaurant");
+    expect(createRestaurantButton).not.toBeInTheDocument();
+
+    const name = screen.getByText("Freebirds");
+    expect(name).toBeInTheDocument();
+
+    const description = screen.getByText(
+      "Burrito joint, and iconic Isla Vista location",
+    );
+    expect(description).toBeInTheDocument();
+
+    // for non-admin users, details button is visible, but the edit and delete buttons should not be visible
+    expect(
+      screen.queryByTestId("RestaurantTable-cell-row-0-col-Delete-button"),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByTestId("RestaurantTable-cell-row-0-col-Edit-button"),
+    ).not.toBeInTheDocument();
+  });
+
+  test("renders empty table when backend unavailable, user only", async () => {
+    setupUserOnly();
+
+    axiosMock.onGet("/api/restaurants/all").timeout();
+
+    const restoreConsole = mockConsole();
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <RestaurantIndexPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1);
+    });
+
+    const errorMessage = console.error.mock.calls[0][0];
+    expect(errorMessage).toMatch(
+      "Error communicating with backend via GET on /api/restaurants/all",
+    );
+    restoreConsole();
+  });
+
+  test("what happens when you click delete, admin", async () => {
+    setupAdminUser();
+
+    axiosMock
+      .onGet("/api/restaurants/all")
+      .reply(200, restaurantFixtures.threeRestaurants);
+    axiosMock
+      .onDelete("/api/restaurants")
+      .reply(200, "Restaurant with id 1 was deleted");
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <RestaurantIndexPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(
+        screen.getByTestId(`${testId}-cell-row-0-col-id`),
+      ).toBeInTheDocument();
+    });
+
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
+      "2",
+    );
+
+    const deleteButton = screen.getByTestId(
+      `${testId}-cell-row-0-col-Delete-button`,
+    );
+    expect(deleteButton).toBeInTheDocument();
+
+    fireEvent.click(deleteButton);
+
+    await waitFor(() => {
+      expect(mockToast).toBeCalledWith("Restaurant with id 1 was deleted");
+    });
+
+    await waitFor(() => {
+      expect(axiosMock.history.delete.length).toBe(1);
+    });
+    expect(axiosMock.history.delete[0].url).toBe("/api/restaurants");
+    expect(axiosMock.history.delete[0].url).toBe("/api/restaurants");
+    expect(axiosMock.history.delete[0].params).toEqual({ id: 2 });
   });
 });
