ef2c23cc4b1b80b7ae3da0f0b3224a245611d247
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-19 15:09:37 -0800

    need to wait for table (epic 11 F1.1) to be completed

--- frontend/src/main/pages/Requests/CompletedRequestsPage.js
+++ frontend/src/main/pages/Requests/CompletedRequestsPage.js
@@ -1,11 +1,29 @@
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestTable from "main/components/RecommendationRequests/RecommendationRequestTable";
+import { useCurrentUser } from "main/utils/currentUser";
 
 export default function CompletedRequestsPage() {
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+  const {
+    data: recommendations,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/recommendationrequests/all"],
+    { method: "GET", url: "/api/recommendationrequests/all" },
+    // Stryker disable next-line all : don't test default value of empty list
+    [],
+  );
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Completed Requests page not yet implemented</h1>
+        <h1>Completed Recommendations</h1>
+        <RecommendationRequestTable recommendations={recommendations} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );
