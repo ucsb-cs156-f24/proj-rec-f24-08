357b949b6cc3298866b52e9e97ba05902854f559
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-19 17:48:21 -0800

    added create page story

---
+++ frontend/src/stories/pages/Requests/RequestsCreatePage.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { http, HttpResponse } from "msw";
+
+import RequestsCreatePage from "main/pages/Requests/RequestCreatePage";
+
+export default {
+  title: "pages/Requests/RequestsCreatePage",
+  component: RequestsCreatePage,
+};
+
+const Template = () => <RequestsCreatePage storybook={true} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.userOnly, {
+        status: 200,
+      });
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither, {
+        status: 200,
+      });
+    }),
+    http.post("/api/recommendationrequests/post", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
