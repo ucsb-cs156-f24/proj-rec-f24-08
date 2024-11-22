62b7d28fb027b4328901901e5b3dfb03b005d9a1
Divyani Punj <divyanipunj@ucsb.edu>
2024-11-22 01:34:53 -0800

    added delete and put (plus tests).

---
+++ src/main/java/edu/ucsb/cs156/rec/entities/RecommendationRequest.java
@@ -0,0 +1,43 @@
+package edu.ucsb.cs156.rec.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * This is a JPA entity that represents a Recommendation Request
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "recommendationrequest")
+public class RecommendationRequest {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+  private User user;
+
+  private String professorName;
+  private String professorEmail;
+  private String requesterName;
+  private String recommendationTypes;
+  private String details;
+  private String status;
+
+  private LocalDateTime submissionDate;
+  private LocalDateTime completionDate;
+}
